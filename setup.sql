3f7441bed8b111a24ee85445dd4ed12f65471d94
Adishree Das <89429561+dasadishree@users.noreply.github.com>
2025-08-14 09:17:47 -0400

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE challenge;
+
+USE challenge;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50)
+);
+
+INSERT INTO users (username, password) VALUES
+('admin', 'supersecret'),
+('guest', 'guestpass');
